84c1e61a2157263fe785c864d88cbfbc6b45c573
Justin <jwhittington92@yahoo.com>
2018-03-13 11:31:36 -0400

    psychic-game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,21 @@
+
+
+//Comments
+// I had to set lives == -1 or else it wouldnt count down properly. Dont know why.
+// After resetGlobalBariables runs, the mouse needs to be clicked an extra time to run the game again. I couldnt figure out how to just make it restart
+// automatically. 
+// The guesslog shows the comma before the letter in the browser
+//=======================================================================================================
+
+
+// Variables
 var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var encouragement = ["Only dead fish go with the flow.", "Zombies eat brains. You're safe.", "In the end, we only regret the chances we didn't take and all our poorly timed farts.","If you punch someone at exactly 11:59:59pm on a saturday night, you will have knocked them into next week."]
+var encouragement = ["Social anxiety is basically Conspiracy Theories about yourself.","If wookies have a 400 year life span, then Han Solo is basically like Chewbacca's third dog",
+                    "Anybody that questions why you are shoveling six inches of snow in the *middle* of a snowstorm hasn't shoveled twelve inches of snow at the end of a snowstorm.",
+                    'When medication says \"do not operate heavy machinery\" they\'re probably mainly referring to cars, but my mind always goes to forklift.',"At the age of 60, Snoop Dogg will be 420 in dog years",
+                    'The olympics is the only time when you hear, "Great execution by North Korea" and it seems okay.',"It kinda makes sense that the target audience for fidget spinners lost interest in them so quickly",
+                    'Somebody at google was just like "yea, just have someone drive down every road on earth".',"Only dead fish go with the flow.", "Zombies eat brains. You're safe.", "In the end, we only regret the chances we didn't take and all our poorly timed farts.",
+                    "If you punch someone at exactly 11:59:59pm on a saturday night, you will have knocked them into next week."]
 var wordsToLiveBy = encouragement[Math.floor(Math.random() * encouragement.length)];
 var lives = 10;
 var winTotal = 0;
@@ -8,12 +24,14 @@ var guessesMade = 0;
 var gameWon = false;
 var gameState = "";
 var guessLog = [];
-var computerPick = computerChoices[Math.floor(Math.random() * computerChoices.length)];           
+var computerPick = computerChoices[Math.floor(Math.random() * computerChoices.length)];   
 
-console.log(computerPick);
+var playitAgain = document.querySelector(".shutupAndPlayTheHits");
 var alreadyGuessed = document.querySelector(".guesses");
-
 var output = document.querySelector(".highLow");
+
+
+
 
 
 
@@ -22,7 +40,7 @@ document.addEventListener("keypress", clickHandler, false);
 document.addEventListener("click", clickHandler, false);
 
 function clickHandler() {
-    playGame();
+    playGame(); 
 }
 
 function playGame() {
@@ -46,62 +64,42 @@ function playGame() {
         endGame();
     }
 
-    if (lives === 0) { 
+    
+    if (lives == -1) { 
         endGame();
     }
 
     function endGame() {
         if (gameWon) {
-            alert("You win!");
+            alert("You win! HIP-HIP-HOORAY! WHOOPIE! YAAAAaay...alright....cool...awesome..............");
             winTotal++;
             resetGlobalVariables();
             
         } else {
-            alert("You lose. The letter was " + computerPick + ".");
+            alert("You lose. The letter was " + computerPick);
             lossTotal++;
             resetGlobalVariables(); 
         }
     }
 
+
+
+
     function resetGlobalVariables() {
+        playGame();
         lives = 10;
         guessesMade = 0;
         computerPick = computerChoices[Math.floor(Math.random() * computerChoices.length)];
         wordsToLiveBy = encouragement[Math.floor(Math.random() * encouragement.length)];
         gameWon = false;
         guessLog = [];
-        
+       
     }
-    console.log(userGuess);
-    console.log(lives);
+    
 }
 
 
-/* document.onkeypress = function(event) {
-    var outputGuess = document.querySelector(".alreadyGuessed");
-    var userGuess = event.key;
-    var computerPick = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    
-    console.log(lives);
-    console.log(computerPick);
-    //for (i = lives; i < 1; i + 5) {
-        if (userGuess == computerPick) {
-            alert("Correct");
-            
-        } else if (userGuess != computerPick) {
-            lives--;   
-          //  outputGuess = userGuess;
-        } 
 
-        if (lives == 0) {
-            alert("Game Over");
-            lives + 5;
-        }
-
-
-    }
-*/
-   
     
     
     
